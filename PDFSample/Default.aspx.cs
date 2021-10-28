8089271f7e39ff101664f19a0a58021f7006e614
Jeff Martinez <jeffxmartinez@gmail.com>
2021-10-27 20:47:06 -0700

    Add project files.

---
+++ PDFSample/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PDFSample
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            SelectPdf.PdfDocument document = new SelectPdf.PdfDocument();
+            SelectPdf.HtmlToPdf htmlToPdf = new SelectPdf.HtmlToPdf();
+            document = htmlToPdf.ConvertUrl(txtUrl.Text);
+            document.Save(Response, false, "Sample.pdf");
+
+            document.Close();
+        }
+    }
+}
